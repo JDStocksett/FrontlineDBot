ac3d5a5ed8507a83be07f913cdf4fb7f4959d3ca
JDStocksett <jdstocksett@gmail.com>
2018-03-31 01:17:42 -0500

    Initial Setup
    First crack at a discord bot

---
+++ bot.js
@@ -0,0 +1,92 @@
+const Discord = require("discord.js");
+//const logger = require('winston');
+const auth = require("./auth.json");
+const prefix = auth.prefix;
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    console.log(`Bot is ready! ${bot.user.username}`);
+
+    /*
+    bot.generateInvite(["ADMINISTRATOR"]).then(link => {
+        console.log(link);
+    }).catch(err => {
+        console.log(err.stack);
+    });
+    console.log("Hello 2");
+    */
+
+    try {
+        let link = await bot.generateInvite(["ADMINISTRATOR"]);
+        console.log(link);
+    } catch(e) {
+        console.log(e.stack);
+    }
+
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    //let args = args.splice(1);
+    
+    if(!command.startsWith(prefix)) return;
+
+    if(command === `${prefix}userinfo`) {
+        let embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username)
+            .setDescription("This is the user's info");
+
+        message.channel.send(embed);
+    }
+
+});
+
+bot.login(auth.token);
+
+
+
+/*
+// console.log(auth.token)
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+*/
