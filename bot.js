66274b7df624631a02d6c9ab9839e9c6fe57ae45
JDStocksett <jdstocksett@gmail.com>
2018-03-31 14:23:45 -0500

    Gifme features

--- bot.js
+++ bot.js
@@ -8,15 +8,6 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
     console.log(`Bot is ready! ${bot.user.username}`);
 
-    /*
-    bot.generateInvite(["ADMINISTRATOR"]).then(link => {
-        console.log(link);
-    }).catch(err => {
-        console.log(err.stack);
-    });
-    console.log("Hello 2");
-    */
-
     try {
         let link = await bot.generateInvite(["ADMINISTRATOR"]);
         console.log(link);
@@ -37,69 +28,107 @@ bot.on("message", async message => {
     console.log(`${messageArray[0]}|${messageArray[1]}`);
     
     if(!command.startsWith(prefix)) return;
+    
+    switch(command) {
 
-    console.log(`Checking for user info`);
-    
-    if(command === `${prefix}userinfo`) {
-        
-        console.log("In user info");
-        
-        let embed = new Discord.RichEmbed()
-            .setAuthor(message.author.username)
-            .setDescription("This is the user's info")
-            .setColor("#9B59B6")
-            .addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
-            .addField("Created Date", `${message.author.createdAt}`)
+        case `${prefix}userinfo`:            
+            let embed = new Discord.RichEmbed()
+                .setAuthor(message.author.username)
+                .setDescription("This is the user's info")
+                .setColor("#9B59B6")
+                .addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
+                .addField("Created Date", `${message.author.createdAt}`)
 
-        message.channel.send(embed);
-        return;
-    };
-    
-    console.log("About to check ping");
-    
-    if(command === `${prefix}ping` || command === `${prefix}Ping`) {
-        console.log("Found ping");
-        message.channel.send("Pong!");
+            message.channel.send(embed);
+            break;
 
-        return;
-    };
-    
-    console.log("Checking weather");
-    
-    if(command === `${prefix}weather`) {
+        case `${prefix}ping`:
+        case `${prefix}Ping`:
+            console.log("Found ping");
+            message.channel.send("Pong!");
+            break;
         
-        console.log("Checking zip");
-        
-        let zipCode = messageArray[1];
-        
-        console.log(`Zip is ${zipCode}`);
-        
-        var request = require("request");
+        case `${prefix}weather`:
+            console.log("Checking zip");
+            
+            let zipCode = messageArray[1];
+            
+            console.log(`Zip is ${zipCode}`);
+            
+            var request = require("request");
 
-        var options = { method: 'GET',
-          url: 'http://api.openweathermap.org/data/2.5/weather',
-          qs: 
-           { zip: `${zipCode},us`,
-             appid: 'c9b9747a481872999cf199acc6bec4ff',
-             units: 'imperial' }
+            var options = { method: 'GET',
+              url: 'http://api.openweathermap.org/data/2.5/weather',
+              qs: 
+               { zip: `${zipCode},us`,
+                 appid: 'c9b9747a481872999cf199acc6bec4ff',
+                 units: 'imperial' }
+                }
+
+            console.log("Making request");
+
+            request(options, function (error, response, body) {
+              if (error) throw new Error(error);
+
+              console.log(body);
+
+              let jsonResponse = JSON.parse(body);
+
+              message.channel.send(`The weather is currently ${jsonResponse.main.temp} degrees in ${jsonResponse.name}.`);
+            })    
+            break;
+
+        case `${prefix}gifme`:
+            let tag = message.content.substring(8,message.content.length);
+
+            console.log(tag);
+
+            var request = require("request");
+
+            var options = { method: 'GET',
+              url: 'http://api.giphy.com/v1/gifs/random',
+              qs: { tag: `${tag}`, api_key: '33RGz8gaeEWlZBnitnrtjD261dzk5iyj' }
             }
+              
+            let msg = await message.channel.send("Generating...");
 
-        console.log("Making request");
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
 
-        request(options, function (error, response, body) {
-          if (error) throw new Error(error);
+                let jsonResponse = JSON.parse(body);
+                let embedURL = jsonResponse.data.embed_url;
+                let gifURL = jsonResponse.data.images.original.url;
+                
+                let msgEmbed = new Discord.RichEmbed()
+                    .setURL(embedURL)
+                    .setImage(gifURL)
+                    .setTitle(`${message.author.username}'s ${tag} gif:`)
 
-          console.log(body);
-
-          let jsonResponse = JSON.parse(body);
-
-          message.channel.send(`The weather is currently ${jsonResponse.main.temp} degrees in ${jsonResponse.name}.`);
-        })    
-
-    return;
-    };
-
+                try{
+                    message.channel.send({embed: msgEmbed});
+                } catch (e) {
+                    console.log(e.stack);
+                }
+                    /*
+                    try {
+                        message.channel.send({embed:{
+                            title:`Your ${tag} gif`,
+                            url:`${embedURL}`,
+                            image: {
+                                "url": `${gifURL}`
+                            
+                        }}});
+                    } catch(e) {
+                       console.log(e.stack);
+                    }
+                    */
+                msg.delete();
+            });
+            break;
+        
+    }
 });
+
 
 bot.login(botSettings.token);
 
